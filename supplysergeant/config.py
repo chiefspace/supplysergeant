6266f5893a3af9880997a8c548c2bdb7c8ac73ca
Ben Altieri <chiefspace@gmail.com>
2017-09-25 23:59:05 +0000

    Added unit test for item assignment date format

--- supplysergeant/config.py
+++ supplysergeant/config.py
@@ -5,3 +5,8 @@ class DevelopmentConfig(object):
     SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:P@ssw0rd@localhost:5432/supplysergeant"
     DEBUG = True
     SECRET_KEY = os.environ.get("SUPPLY_SERGEANT_SECRET_KEY", os.urandom(12))
+    
+class TestingConfig(object):
+    SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:P@ssw0rd@localhost:5432/supplysergent-test"
+    DEBUG = False
+    SECRET_KEY = "Not secret"
